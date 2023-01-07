95cd38f678a5678ad55cfe2f1b5dfa4cada509df
djey47 <djey47@github.com>
2023-01-07 11:48:30 +0100

    feature(helper): introduce custom processor as sample

---
+++ src/server/processing/custom-processor.ts
@@ -0,0 +1,14 @@
+import * as Log from 'logger';
+import { MM2Helper } from '../types/mm2';
+
+export function startProcessing(moduleContext: MM2Helper) {
+  Log.log('**** custom-processor:startProcessing', moduleContext);
+
+  let iterationIndex = 0;
+  setInterval(() => {
+    if (moduleContext.sendSocketNotification) {
+      moduleContext.sendSocketNotification('NOTIF', { iterationIndex });
+    }
+    iterationIndex += 1;
+  }, 5000);
+}
