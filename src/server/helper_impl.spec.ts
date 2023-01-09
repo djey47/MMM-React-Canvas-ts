46d2f0a955a90baff85168af2613c9de193c15c1
djey47 <djey47@github.com>
2023-01-09 20:24:09 +0100

    test(helper): add unit test

---
+++ src/server/helper_impl.spec.ts
@@ -0,0 +1,57 @@
+jest.mock('./utils/mm2_facades', () => ({
+  Log: {
+    log: jest.fn(),
+  },
+  NodeHelper: {},
+}));
+
+const mockStartProcessing = jest.fn();
+jest.mock('./processing/custom-processor', () => ({
+  startProcessing: mockStartProcessing,
+}));
+
+import * as impl from './helper_impl';
+import { MM2Helper } from './types/mm2'
+
+const nodeHelper = impl as MM2Helper;
+
+describe('MM2 helper implementation', () => {
+  describe('start function', () => {
+    it('should set started flag to false', () => {
+      // given-when
+      nodeHelper.start();
+
+      // then
+      expect(nodeHelper.started).toBe(false);
+    });
+  });  
+  
+  describe('socketNotificationReceived function', () => {
+    beforeEach(() => {
+      mockStartProcessing.mockReset();
+    });
+
+    it('should do nothing when notif is not SET_CONFIG', () => {
+      // given-when
+      nodeHelper.socketNotificationReceived('NOTIF', {});
+
+      // then
+      expect(mockStartProcessing).not.toHaveBeenCalled();
+    });    
+    
+    it('should register config when notif is SET_CONFIG', () => {
+      // given
+      const sendSocketNotificationMock = jest.fn();
+      nodeHelper.sendSocketNotification = sendSocketNotificationMock;
+
+      // when
+      nodeHelper.socketNotificationReceived('SET_CONFIG', { key1: 'value1' });
+
+      // then
+      expect(nodeHelper.config).toEqual({ key1: 'value1' });
+      expect(nodeHelper.started).toBe(true);
+      expect(sendSocketNotificationMock).toHaveBeenCalledWith('INIT');
+      expect(mockStartProcessing).toHaveBeenCalledWith(nodeHelper);
+    });
+  });
+});
