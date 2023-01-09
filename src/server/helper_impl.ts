6044484d177da1d5173886e3639a24b51fd1ac50
djey47 <djey47@github.com>
2023-01-09 23:22:52 +0100

    refactor(helper): remove debug logs, use debug mode

--- src/server/helper_impl.ts
+++ src/server/helper_impl.ts
@@ -1,4 +1,3 @@
-import { Log } from './utils/mm2_facades';
 import {
   NOTIF_INIT,
   NOTIF_SET_CONFIG,
@@ -7,17 +6,15 @@ import { MM2Helper } from './types/mm2';
 import { startProcessing } from './processing/custom-processor';
 
 /**
- * Magic Mirror
+ * Magic Mirror 2
  * Custom NodeHelper implementation
  */
 const mm2Helper: MM2Helper = {
   start: function() {
-    Log.log('**** mm2Helper:start');
     this.started = false;
   },
 
   socketNotificationReceived: function(notification: string, payload: object) {
-    Log.log('**** mm2Helper:socketNotificationReceived', { notification, payload });
     if (notification === NOTIF_SET_CONFIG && !this.started) {
       this.config = payload;
       this.started = true;
--- src/server/processing/custom-processor.ts
+++ src/server/processing/custom-processor.ts
@@ -1,13 +1,15 @@
 import { Log } from '../utils/mm2_facades';
 import { MM2Helper } from '../types/mm2';
 
-export function startProcessing(moduleContext: MM2Helper) {
-  Log.log('**** custom-processor:startProcessing', moduleContext);
+export function startProcessing(helperContext: MM2Helper) {
+  if (helperContext.config?.debug) {
+    Log.log('**** custom-processor:startProcessing', helperContext);
+  }
 
   let iterationIndex = 0;
   setInterval(() => {
-    if (moduleContext.sendSocketNotification) {
-      moduleContext.sendSocketNotification('NOTIF', { iterationIndex });
+    if (helperContext.sendSocketNotification) {
+      helperContext.sendSocketNotification('NOTIF', { iterationIndex });
     }
     iterationIndex += 1;
   }, 5000);
