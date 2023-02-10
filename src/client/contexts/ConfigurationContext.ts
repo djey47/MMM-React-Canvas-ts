b04ae96caede84c6398026d28a6fced46d9886b4
djey47 <djey47@github.com>
2023-02-10 15:53:13 +0100

    chore(lint): replace double quotes with simple ones

--- src/client/contexts/ConfigurationContext.ts
+++ src/client/contexts/ConfigurationContext.ts
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext } from 'react';
 
 const ConfigurationContext = createContext<ModuleConfiguration | undefined>({});
 export default ConfigurationContext;
